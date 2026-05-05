d65ebdaaf24e4e5e99346b929c77059dd463a2bc
areej-alghamdi <areejaa12@gmail.com>
2026-05-05 21:21:19 +0000

    Add key_index function

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gets the index of a key
+ * @key: The key to get the index of
+ * @size: The size of the hash table
+ *
+ * Return: The index where the key should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
