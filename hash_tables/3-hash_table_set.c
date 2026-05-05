18d42b68f466649aefbc187ce044507f54447356
areej-alghamdi <areejaa12@gmail.com>
2026-05-05 21:36:58 +0000

    Fix Betty style split into helper function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,34 @@
 #include <stdlib.h>
 
 /**
+ * update_value - Updates value if key already exists
+ * @node: The node to check
+ * @key: The key to look for
+ * @value: The new value
+ *
+ * Return: 1 if updated, 0 if not found
+ */
+int update_value(hash_node_t *node, const char *key, const char *value)
+{
+	char *new_value;
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(node->value);
+			node->value = new_value;
+			return (1);
+		}
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table to add the key/value to
  * @key: The key - cannot be an empty string
@@ -13,29 +41,15 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	hash_node_t *tmp;
 	unsigned long int index;
-	char *new_value;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
-		}
-		tmp = tmp->next;
-	}
+	if (update_value(ht->array[index], key, value))
+		return (1);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
